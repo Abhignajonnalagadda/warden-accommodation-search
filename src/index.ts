7f18e4d488a63435a21733c4999986bda36eb396
Abhigna-Jonnalagadda <abhigna.jonnalagadda@testsigma.com>
2025-08-26 18:33:06 +0530

    fix: CORS error

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 import { getProperties } from "./use-cases/getProperties";
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET"],
+    credentials: true,
+  })
+);
 
 app.get("/", (_req, res) => res.send("Warden Weather Test: OK"));
 app.use(`/get-properties`, getProperties);
